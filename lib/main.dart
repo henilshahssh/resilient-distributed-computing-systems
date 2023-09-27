da6e679f2e22f1a157946d3f206950c429eecedf
krats-win <100959920+krats-win@users.noreply.github.com>
2023-09-27 17:48:22 +1000

    Menu screen contents moved from main.dart to menu.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sepb_web_app/screens/menu.dart';
 import 'package:sepb_web_app/util/constants.dart';
 import 'package:sepb_web_app/util/helperFunctions.dart';
 import 'package:sepb_web_app/widgets/cardCustomized.dart';
@@ -32,49 +33,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: backgroundScaffoldColor,
-      body: Column(
-        children: [
-          const NavBar(),
-          Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.all(20),
-                child: CardCustomized(
-                  width: getScreenWidth(context),
-                  height: getScreenHeight(context) * 0.4,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.only(left: 25, top: 20),
-                        child: FormattedText(
-                          text: "New system",
-                          fontSize: 24,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(left: 40, top: 20, bottom: 20),
-                        child: const CardCustomized(
-                          child: Center(
-                            child: Icon(
-                              Icons.add_rounded,
-                              size: 60,
-                              color: primaryColor,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+    return const Menu();
   }
 }
